b4e155f995c0e1ebed974bb05a1012364e3199d9
Mohammad Reza Omrani <52138433+mohammad-reza-omrani@users.noreply.github.com>
2024-08-11 22:48:11 +0200

    add
    file

---
+++ 1.10/DATABASECONNECTION/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const HomePageSchema = new Schema({
+color: {
+type: String,
+},
+});
+const HomePage = mongoose.model('homepages', HomePageSchema);
+
+const DB_URI ="mongodb+srv://dbmohammd:eIsm1Du5dGGb2jDk@electron.y2r5l.mongodb.net/Website?retryWrites=true&w=majority"; 
+mongoose.connect(DB_URI, {});
+
+app.set('view engine', 'ejs');
+
+app.get("/", () => {
+res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => {});
